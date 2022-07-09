f017883002771ff4a9b020d53aef2a03bf53899e
faisolrr <faisolrizkiromdon@gmail.com>
2022-07-09 11:49:22 +0700

    melengkapi tugas

--- Part1/problem1/problem1.js
+++ Part1/problem1/problem1.js
@@ -1,5 +1,27 @@
 function simpleEquations(a, b, c) {
   // Your code here
+  let result=""
+  let x=1;
+  let y=0;
+  let z=0;
+  for (let i=b-1; i>=1; i--){
+    if (b%i==0){
+      y=i
+      z=b/i
+    }
+    if ((x+y+z)==a && ((x*x)+(y*y)+(z*z)==c)){
+        result=x+" "+y+" "+z
+    }
+  }
+  if (result==""){
+    return "no solution"
+  }
+  return result;
 }
+console.log(simpleEquations(1, 2, 3));  // no solution
+console.log(simpleEquations(6, 6, 14));  // 1 2 3
+
+simpleEquations(1, 2, 3)  // no solution
+simpleEquations(6, 6, 14) // 1 2 3
 
 module.exports = simpleEquations;
--- Part1/problem3/problem3.js
+++ Part1/problem3/problem3.js
@@ -1,5 +1,27 @@
 function dragonOfLoowater(dragonHead, knightHeight) {
   //  Your code here
-}
+  let result=0;
+  let knightHeighttmp=knightHeight.sort()
+  for (let i=0; i<dragonHead.length; i++){
+    let found=false;
+    for (let j = 0; j < knightHeighttmp.length; j++) {
+      if (dragonHead[i]<=knightHeighttmp[j]){
+        result+=knightHeighttmp[j];
+        knightHeighttmp.splice(j,1);
+        found=true;
+        break
+      }
+    }
+    if (found==false){
+      return "knight fall"
+    }
+  }
+  return result;
+  }
+
+  console.log(dragonOfLoowater([5, 4], [7, 8, 4]));
+  console.log(dragonOfLoowater([5, 10], [5]));
+  console.log(dragonOfLoowater([7, 2], [4, 3, 1, 2]));
+  console.log(dragonOfLoowater([7, 2], [2, 1, 8, 5]));
 
 module.exports = dragonOfLoowater;
